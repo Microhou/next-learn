47f2a67c4c1f07c286b5a1bfeac2f8d9c39838dc
anying <372931530@qq.com>
2024-12-15 22:18:06 +0800

    add find api code

--- app/api/articles/[id]/route.ts
+++ app/api/articles/[id]/route.ts
@@ -25,11 +25,33 @@ export async function PUT(request: Request, { params }: { params: { id: string }
     
 }
 
-export async function PATCH(request: Request, { params }: { params: { id: string } }) {
-    
+export async function PATCH(request: Request, { params }: IParams) {
+    const data = await request.json();
+    const { id } = params;
+    let idx = -1;
+    await db.update(({ posts }) => {
+        idx = posts.findIndex((post) => post.id === id);
+        posts[idx] = {
+            ...posts[idx],
+            ...data
+        }
+    })
+
+    return NextResponse.json({
+        code: 0,
+        message: "updated success",
+        data: db.data.posts[idx]
+    })
+
 }
 
 // get ==> api/articles/:id
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-    
+export async function GET(request: Request, { params }: IParams) {
+    const { id } = params;
+    const post = db.data.posts.find((post) => post.id === id);
+    return NextResponse.json({
+        code: 0,
+        message: "success",
+        data: post
+    })
 }
